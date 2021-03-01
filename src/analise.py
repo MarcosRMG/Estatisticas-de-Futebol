be6a6e97654e8fc4d5a9dbaca2651eac0ed5ba1c
Marcos Galvão <marcos_rmgalvao@hotmail.com>
2021-03-01 20:44:42 -0300

    Adicionando o número de escanteios

--- src/analise.py
+++ src/analise.py
@@ -8,12 +8,15 @@ def indicadores(dados: pd.DataFrame, ultimos_jogos=5):
     :param dados: DataFrame com o números das rodadas da liga
     :param ultimos_jogos: Seleção do número de jogos anteriores para cálculo dos indicadores
     '''
-    st.markdown('Gols marcados')
+    st.markdown('GOLS MARCADOS')
     st.text('Média:' + ' ' + str(int(dados['gols_marcados'][:ultimos_jogos].mean().round())))
     st.text('Variação +-' + ' ' + str(int(dados['gols_marcados'][:ultimos_jogos].std().round())))
-    st.markdown('Gols sofridos')
+    st.markdown('GOLS SOFRIDOS')
     st.text('Média:' + ' ' + str(int(dados['gols_sofridos'][:ultimos_jogos].mean().round())))
     st.text('Variação +-' + ' ' + str(int(dados['gols_sofridos'][:ultimos_jogos].std().round())))
-    st.markdown('Posse de bola')
+    st.markdown('POSSE DE BOLA')
     st.text('Média:' + ' ' + str(int(dados['posse'][:ultimos_jogos].mean().round())) + '%')
     st.text('Variação +-' + ' ' + str(int(dados['posse'][:ultimos_jogos].std().round())) + '%')
+    st.markdown('ESCANTEIOS')
+    st.text('Média:' + ' ' + str(int(dados['escanteios'][:ultimos_jogos].mean().round())))
+    st.text('Variação +-' + ' ' + str(int(dados['escanteios'][:ultimos_jogos].std().round())))
