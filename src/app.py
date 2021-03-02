5ccc8977eebfd1771f7f279b27f7c45d3c96c735
Marcos Galvão <marcos_rmgalvao@hotmail.com>
2021-03-02 10:26:00 -0300

    Filtros local de jogo

--- src/app.py
+++ src/app.py
@@ -16,15 +16,31 @@ def main():
     ultimos_jogos = st.selectbox('Últimos Jogos', numero_jogos)
     col_1, col_2 = st.beta_columns(2)
     with col_1:
-        equipe_1 = st.selectbox('Equipe Mandante', tabela_liga_italia_21['equipe'].values)
-        st.image(rodadas_liga_italia_21.query('clube == @equipe_1')['escudo'].iloc[-1])
-        indicadores(rodadas_liga_italia_21.query('clube == @equipe_1'), ultimos_jogos=ultimos_jogos)
-        st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_1').iloc[:ultimos_jogos, :-1])
+        # Filtros
+        equipe_1 = st.selectbox('Equipe 1', tabela_liga_italia_21['equipe'].values)
+        local_equipe_1 = st.selectbox('Local Equipe 1', ['Ambos', 'Em casa', 'Visitante'])
+        # Visualizações
+        if local_equipe_1 == 'Ambos':
+            st.image(rodadas_liga_italia_21.query('clube == @equipe_1')['escudo'].iloc[-1])
+            indicadores(rodadas_liga_italia_21.query('clube == @equipe_1'), ultimos_jogos=ultimos_jogos)
+            st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_1').iloc[:ultimos_jogos, :-1])
+        elif local_equipe_1 != 'Ambos':
+            st.image(rodadas_liga_italia_21.query('clube == @equipe_1')['escudo'].iloc[-1])
+            indicadores(rodadas_liga_italia_21.query('clube == @equipe_1 and local == @local_equipe_1'), ultimos_jogos=ultimos_jogos)
+            st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_1 and local == @local_equipe_1').iloc[:ultimos_jogos, :-1])
     with col_2:
-        equipe_2 = st.selectbox('Equipe Visitante', tabela_liga_italia_21['equipe'][1:].values)
-        st.image(rodadas_liga_italia_21.query('clube == @equipe_2')['escudo'].iloc[-1])
-        indicadores(rodadas_liga_italia_21.query('clube == @equipe_2'), ultimos_jogos=ultimos_jogos)
-        st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_2').iloc[:ultimos_jogos, :-1])
+        # Filtros
+        equipe_2 = st.selectbox('Equipe 2', tabela_liga_italia_21['equipe'][1:].values)
+        local_equipe_2 = st.selectbox('Local Equipe 2', ['Ambos', 'Em casa', 'Visitante'])
+        # Visualizações
+        if local_equipe_2 == 'Ambos':
+            st.image(rodadas_liga_italia_21.query('clube == @equipe_2')['escudo'].iloc[-1])
+            indicadores(rodadas_liga_italia_21.query('clube == @equipe_2'), ultimos_jogos=ultimos_jogos)
+            st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_2').iloc[:ultimos_jogos, :-1])
+        elif local_equipe_2 != 'Ambos':
+            st.image(rodadas_liga_italia_21.query('clube == @equipe_2')['escudo'].iloc[-1])
+            indicadores(rodadas_liga_italia_21.query('clube == @equipe_2 and local == @local_equipe_2'), ultimos_jogos=ultimos_jogos)
+            st.dataframe(rodadas_liga_italia_21.query('clube == @equipe_2 and local == @local_equipe_2').iloc[:ultimos_jogos, :-1])
 
 if __name__ == '__main__':
     main()
